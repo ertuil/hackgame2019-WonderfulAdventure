3e97ba44d799ebbc8f90527da17027123de26db5
ertuil <ertuil@ertuil.top>
2019-07-26 17:24:51 +0800

    change build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
 go-bindata -o=./asset/asset.go -pkg=asset statics/...
-go build -o public/wa WonderfulAdventure && cd public &&  ./wa
+go build -o public/wa-darwin WonderfulAdventure
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o bin/wa-linux WonderfulAdventure
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o bin/wa-windows.exe WonderfulAdventure
+cd public &&  ./wa-darwin
